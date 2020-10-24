d2fee4b10c2be53fda79d9cc8da03b1c93d948ab
Alexander Ulmer <ulmer@student.tugraz.at>
2020-10-24 11:25:40 +0200

    update version schema

--- tools/version.sh
+++ tools/version.sh
@@ -3,11 +3,12 @@
 vpath="kernel/include/version.h"
 
 os_name="Ulmer OS"
-os_version=`git rev-list --count master`
+os_maj_version="0"
+os_min_version=`git rev-list --count master`
 os_author="Alexander Ulmer"
 os_build_date=`date --utc --rfc-3339=date`
 
 echo "#define OS_NAME \"$os_name\"" > $vpath
-echo "#define OS_VERSION \"v$os_version\"" >> $vpath
+echo "#define OS_VERSION \"v$os_maj_version.$os_min_version\"" >> $vpath
 echo "#define OS_AUTHOR \"$os_author\"" >> $vpath
 echo "#define OS_BUILD_DATE \"$os_build_date\"" >> $vpath

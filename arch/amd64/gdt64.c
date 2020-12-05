7e46d51cb25b78f37d8063dbfe97d0e8ada5b974
Alexander Ulmer <ulmer@student.tugraz.at>
2020-12-05 11:09:15 +0100

    init and load task state segment on boot

--- arch/amd64/gdt64.c
+++ arch/amd64/gdt64.c
@@ -18,8 +18,10 @@
 #define CODESEG     (BIT(11)|BIT(12))
 #define DATASEG     (BIT(12))
 
+#define TSS_INDEX   5
+
 // null, kcode, kdata
-#define GDT_ENTRIES 5
+#define GDT_ENTRIES 7 // 1 null + 4 segment + 2 tss
 
 /* global descriptor table descriptor */
 typedef struct gdtd_struct
@@ -38,6 +40,25 @@ typedef struct gdte_struct
   uint8_t flags1_limit;
   uint8_t base_high;
 } __attribute__((packed)) gdte_t;
+
+typedef struct tss_desc_struct
+{
+  uint64_t limit1     :16;  // bits 0-15
+  uint64_t base1      :24;  // bits 0-23
+  uint64_t type       :4;
+  uint64_t zero1       :1;
+  uint64_t dpl        :2;
+  uint64_t present    :1;
+  uint64_t limit2     :4;   // bits 16-19
+  uint64_t avl        :1;
+  uint64_t reserved1  :2;
+  uint64_t gran       :1;
+  uint64_t base2      :40;   // bits 24-63
+  uint64_t reserved2  :8;
+  uint64_t zero2      :5;
+  uint64_t reserved3  :19;
+
+} __attribute__((packed)) tss_desc_t;
 
 typedef struct tss_struct
 {
@@ -78,6 +99,25 @@ static void setup_entry(int id, uint32_t base, uint32_t limit,
   *((uint32_t*)&(gdt[id].base_middle)) |= flags;
 }
 
+void tss_init(size_t id)
+{
+  size_t addr = (size_t)&tss;
+
+  tss_desc_t* td = (tss_desc_t*)&(gdt[id]);
+  td->zero1   = 0x00;
+  td->zero2   = 0x00;
+  td->base1   = addr & 0xffffff;
+  td->base2   = addr >> 24;
+  td->limit1  = sizeof(tss_t);
+  td->limit2  = 0x00;
+
+  td->present = 0x01; // present
+  td->avl     = 0x00; // not used
+  td->gran    = 0x00; // not scaled
+  td->dpl     = 0x03; // kernel
+  td->type    = 0x09; // available TSS
+}
+
 void gdt_init(void)
 {
   gdt_descriptor.size = sizeof(gdte_t) * GDT_ENTRIES - 1;
@@ -90,8 +130,19 @@ void gdt_init(void)
   setup_entry(3, 0, 0xf0000, PRESENT | GRAN_4K | LONGMODE | ACC_EXC_RD | CODESEG | DPL_USER);  // ucode
   setup_entry(4, 0, 0xf0000, PRESENT | GRAN_4K | LONGMODE | ACC_EXC_RD | DATASEG | DPL_USER);  // udata
 
+  tss_init(TSS_INDEX);
 
-  __asm__ volatile("lgdt %0" : : "g"(gdt_descriptor));
+  __asm__ volatile(
+    "lgdt %0"
+    : : "g"(gdt_descriptor)
+  );
+
+  uint64_t tss_sel = (TSS_INDEX << 3) | 0b011;
+  __asm__ volatile (
+    "mov %0, %%ax;"
+    "ltr %%ax;"
+    : : "g"(tss_sel)
+  );
 }
 
 void arch_init()

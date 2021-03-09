bd2e365dace7c87a87ae6f126004a1eef7b8cc73
Alexander Ulmer <ulmer@student.tugraz.at>
2021-03-09 15:36:07 +0100

    add page manager mutex

--- kernel/kernel/mm/page.c
+++ kernel/kernel/mm/page.c
@@ -5,6 +5,7 @@
 #include <debug.h>
 
 static bitmap_t free_pages;
+static mutex_t free_pages_lock = MUTEX_INITIALIZER;
 
 void setup_page_bitmap(void* bitmap_addr, size_t size)
 {
@@ -14,9 +15,11 @@ void setup_page_bitmap(void* bitmap_addr, size_t size)
 
 size_t alloc_page()
 {
+  mutex_lock(&free_pages_lock);
   size_t ppn = bitmap_find_free(&free_pages);
   kpanic(ppn != (size_t)-1, "alloc_page() out of memory");
   bitmap_set(&free_pages, ppn);
+  mutex_unlock(&free_pages_lock);
   memset(ppn_to_virt(ppn), 0, PAGE_SIZE);
   return ppn;
 }
@@ -25,11 +28,14 @@ void* alloc_dma_region()
 {
   size_t dma_pages = 16;            // 16P = 64k
   size_t max_page = 1048576 - 1;    // 1MP = 4GB
+
+  mutex_lock(&free_pages_lock);
   size_t ppn_start = bitmap_find_n_free(&free_pages, dma_pages);
 
   /* validate */
   if (ppn_start == (size_t)-1 || ppn_start + dma_pages > max_page)
   {
+    mutex_unlock(&free_pages_lock);
     kpanic(false, "No DMA region could be allocated!");
     return NULL;
   }
@@ -39,6 +45,7 @@ void* alloc_dma_region()
   while (dma_pages--)
     bitmap_set(&free_pages, page++);
 
+  mutex_unlock(&free_pages_lock);
   return (void*)(ppn_start << PAGE_SHIFT);
 }
 
@@ -49,6 +56,8 @@ void free_page(size_t page)
     assert(false, "page out of bounds");
     return;
   }
+  mutex_lock(&free_pages_lock);
   assert(bitmap_get(&free_pages, page) == 1, "double free page");
   bitmap_clr(&free_pages, page);
+  mutex_unlock(&free_pages_lock);
 }
